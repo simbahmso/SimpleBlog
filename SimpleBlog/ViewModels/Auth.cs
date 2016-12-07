d8043dfe83ac7834d701e139f66351091357d5b5
DESKTOP-OART1FI\simba <simbahmso@yahoo.com>
2016-12-07 11:47:04 -0800

    Set Password field to hide encypt(hide) password by setting data annotation in  Auth.cs class - Can no longer see actual password typed

--- SimpleBlog/ViewModels/Auth.cs
+++ SimpleBlog/ViewModels/Auth.cs
@@ -1,10 +1,14 @@
-﻿namespace SimpleBlog.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.ViewModels
 {
     public class AuthLogin
     {
         public string Test { get; set; }
-
+        
         public string Username { get; set; }
+
+        [DataType(DataType.Password)]
         public string Password { get; set; }  
     }
 }
