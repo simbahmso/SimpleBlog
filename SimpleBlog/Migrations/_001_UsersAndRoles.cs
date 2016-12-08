27a51c3409ecc6db0366f5dbf4823d58e3a63eb9
DESKTOP-OART1FI\simba <simbahmso@yahoo.com>
2016-12-08 12:19:54 -0800

    14. Versioning our Database with Fluent Migrator Managing Database within code - FluentMigrator will manage database for us - Code First created Connection String in Web.config file that points to our MySQL Database - 3 tables created from powershell cli => users, roles, role-users

---
+++ SimpleBlog/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+
+namespace SimpleBlog.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(128)
+                .WithColumn("email").AsString(256)
+                .WithColumn("password_hash").AsString(128);
+
+            Create.Table("roles")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("name").AsString(128);
+
+            Create.Table("role_users")
+                .WithColumn("user_id").AsInt32().ForeignKey("users", "id").OnDelete(Rule.Cascade)
+                .WithColumn("role_id").AsInt32().ForeignKey("roles", "id").OnDelete(Rule.Cascade);
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("users");
+            Delete.Table("roles");
+            Delete.Table("role_users");
+        }
+    }
+}
