0a95a3a2b10fcfdcb68580a840bf5f8931162d54
DESKTOP-OART1FI\simba <simbahmso@yahoo.com>
2016-12-07 18:07:13 -0800

    Scripts, Styles and Bundles, Fonts - from scratch -

--- SimpleBlog/Global.asax.cs
+++ SimpleBlog/Global.asax.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+
 
 namespace SimpleBlog
 {
@@ -13,6 +15,7 @@ namespace SimpleBlog
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
